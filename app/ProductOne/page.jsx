ddb3728baf3d51ed4347b2377a17ba1a2544f0d1
Janine Escobillo <janineescobillo@hotmail.com>
2025-03-30 18:39:26 +0200

    changes price

--- app/ProductOne/page.jsx
+++ app/ProductOne/page.jsx
@@ -3,7 +3,7 @@ const ProductOne = () => {
     <div className="grid grid-rows justify-center items-center">
       <img className="w-50 h-50 object-cover" src="https://i.pinimg.com/736x/27/15/76/27157648e1ad5532f448e7da81188d44.jpg" alt="t-shirt" />
       <h2 className="mt-3">T-shirt</h2>
-      <p>Price: <strong>200 SEK</strong></p>
+      <p>Price: <strong>500 SEK</strong></p>
   </div>
   )
 }
