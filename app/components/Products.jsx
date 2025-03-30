9f12d389f99b4db6cfef7d2e8eb74abfe28198b9
Janine Escobillo <janineescobillo@hotmail.com>
2025-03-30 18:34:51 +0200

    changes price

--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -9,7 +9,7 @@ const Products = () => {
         <div>
           <img className="w-50 h-50 object-cover" src="https://i.pinimg.com/736x/27/15/76/27157648e1ad5532f448e7da81188d44.jpg" alt="t-shirt" />
           <h2 className="mt-3">T-shirt</h2>
-          <p>Price: <strong>200 SEK</strong></p>
+          <p>Price: <strong>500 SEK</strong></p>
         </div>
       </Link>
 
