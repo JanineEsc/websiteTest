93626b0c47a9fb36e0ea7d9f16cc41350ce1a224
Janine Escobillo <janineescobillo@hotmail.com>
2025-03-31 16:52:04 +0200

    adds a product

--- app/ProductOne/page.jsx
+++ app/ProductOne/page.jsx
@@ -4,6 +4,12 @@ const ProductOne = () => {
       <img className="w-50 h-50 object-cover" src="https://i.pinimg.com/736x/27/15/76/27157648e1ad5532f448e7da81188d44.jpg" alt="t-shirt" />
       <h2 className="mt-3">T-shirt</h2>
       <p>Price: <strong>500 SEK</strong></p>
+      
+      <div>
+          <img className="w-50 h-50 object-cover" src="https://i.pinimg.com/736x/5e/81/f0/5e81f0b0690996d301c67abe583349f9.jpg" alt="dress" />
+          <h2 className="mt-3">Dress</h2>
+          <p>Price: <strong>400 SEK</strong></p>
+        </div>
   </div>
   )
 }
--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -14,7 +14,7 @@ const Products = () => {
       </Link>
 
       <Link href="/ProductTwo">
-      <div>
+      <div> 
           <img className="w-50 h-50 object-cover" src="https://i.pinimg.com/736x/5e/81/f0/5e81f0b0690996d301c67abe583349f9.jpg" alt="dress" />
           <h2 className="mt-3">Dress</h2>
           <p>Price: <strong>400 SEK</strong></p>
